2ae2a044a86259a665ee9992681b5b725ca79fd5
Sonund_of_Wind <59632711+xuejy19@users.noreply.github.com>
2023-01-04 18:52:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,13 +52,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0p9LL9Z_CQnfkyXfrWK24dxNXoR8dVj_5zLTkLSDMKc',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-ENC6e9oiQr6uH5keVX3yLKq0C8',
     }
   ],
 
