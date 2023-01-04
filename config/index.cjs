9e54ad22eebaebb3b52a95f97d73f81174ad1ffb
Sonund_of_Wind <59632711+xuejy19@users.noreply.github.com>
2023-01-04 16:11:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx33747521905754a2',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'b056eda9f395fc0ed12a56fec9773aec',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '',https://github.com/xuejy19/wechat-public-account-push/blob/master/config/index.cjs
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
